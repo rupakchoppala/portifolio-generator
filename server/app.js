73b29224949bb69c49ff1089d540cfc7b4b32c93
rupak choppala <rupakchoppala@gmail.com>
2025-03-21 17:55:17 +0530

    frontend deployment

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const app=express();
 //app.use(express.json());
 app.use(
     cors({
-      origin: ["http://localhost:5173", "https://portifolio-generator-z83b.vercel.app/"],
+      origin: ["http://localhost:5173", "https://portifolio-generator-4.onrender.com/"],
       credentials: true, // Allow cookies and auth headers
       methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
     })
