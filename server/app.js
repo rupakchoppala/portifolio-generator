002821bc718900ff6e917781a571a144c6232fc2
rupak choppala <rupakchoppala@gmail.com>
2025-03-21 16:49:14 +0530

    deployed

--- server/app.js
+++ server/app.js
@@ -3,7 +3,13 @@ import fileUpload from "express-fileupload";
 import cors from "cors";
 const app=express();
 //app.use(express.json());
-app.use(cors());
+app.use(
+    cors({
+      origin: ["http://localhost:5173", "https://portifolio-generator-z83b.vercel.app/"],
+      credentials: true, // Allow cookies and auth headers
+      methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+    })
+  );
 app.use(fileUpload({ useTempFiles: true }));
 app.use(express.json({ limit: "50mb" })); // Increase payload size limit
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
