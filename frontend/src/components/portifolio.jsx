c84711afce9d5dbb7d63e867ec7e8c850db857e5
rupak choppala <rupakchoppala@gmail.com>
2025-03-25 12:09:01 +0530

    trying to deploy

--- frontend/src/components/portifolio.jsx
+++ frontend/src/components/portifolio.jsx
@@ -4,10 +4,11 @@ import axios from "axios";
 import Navbar from "./Navbar";
 import Hero from "./Hero";
 import About from "./About";
-import Technologies from "./Technologies";
+import Technoligies from "./Technologies";
 import Experience from "./Experience";
-import Projects from "./Projects";
+import Project from "./Projects";
 import Contact from "./contacts";
+
 
 const UserPortfolio = () => {
   const { id } = useParams(); // Ensure the parameter matches the route path
@@ -49,15 +50,22 @@ const UserPortfolio = () => {
   if (!userData) return <h2>Loading...</h2>;
 
   return (
-    <div className="w-full min-h-screen overflow-x-hidden text-neutral-300 antialiased">
-      <Navbar data={userData} />
-      <Hero data={userData} />
-      <About data={userData} />
-      <Technologies data={userData} />
-      <Experience data={userData} />
-      <Projects data={userData} />
-      <Contact data={userData} />
+    <div className="w-full min-h-screen overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900 font-inter">
+      {/* Background Layer */}
+      <div className=" top-0  h-full w-full"> </div>
+      <div className="absolute top-0  h-auto w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
+        <div className="container mx-auto px-8 h-auto overflow-x-hidden">
+          <Navbar data={userData} />
+          <Hero data={userData} />
+          <About data={userData} />
+          <Technoligies data={userData} />
+          <Experience data={userData} />
+          <Project data={userData} />
+          <Contact data={userData} />
+        </div>
+      </div>
     </div>
+
   );
 };
 
